75993ae33858834e394acb9598ce170f22725689
ezeswci <ezequiel@swci.com.ar>
2018-01-15 17:04:05 -0300

    Nuevo mapa!

--- scripts/gps/maps.js
+++ scripts/gps/maps.js
@@ -15,20 +15,21 @@ function onMapReady() {
 function onButtonClick() {
   alert("Click en boton");
   // Move to the position with animation
+  // (-34.4191274,-58.81449651,-34.418242,-58.815308))
   map.animateCamera({
-    target: {lat: 37.422359, lng: -122.084344},
+    target: {lat: -34.4191274, lng: -58.81449651},
     zoom: 17,
     tilt: 60,
     bearing: 140,
-    duration: 5000
+    duration: 2000
   }, function() {
 
     // Add a maker
     map.addMarker({
-      position: {lat: 37.422359, lng: -122.084344},
-      title: "Welecome to \n" +
-             "Cordova GoogleMaps plugin for iOS and Android",
-      snippet: "This plugin is awesome!",
+      position: {lat: -34.418242, lng: -58.81449651},
+      title: "Nombre de un paciente \n" +
+             "Juan Perez",
+      snippet: "Proxima: 10-02-2018",
       animation: plugin.google.maps.Animation.BOUNCE
     }, function(marker) {
 
@@ -39,9 +40,27 @@ function onButtonClick() {
       marker.on(plugin.google.maps.event.INFO_CLICK, function() {
 
         // To do something...
-        alert("Hello world!");
+        alert("Abre Juan perez");
 
       });
+      map.addMarker({
+        position: {lat: -34.4191274, lng: -58.81449651},
+        title: "Segundo paciente \n" +
+               "Pedro Aznar",
+        snippet: "Proxima: 15-03-2019",
+        animation: plugin.google.maps.Animation.BOUNCE
+      }, function(marker) {
+
+        // Show the info window
+        marker.showInfoWindow();
+
+        // Catch the click event
+        marker.on(plugin.google.maps.event.INFO_CLICK, function() {
+
+          // To do something...
+          alert("Abre Pedro");
+
+        });
     });
   });
 }
